93ff3660b742e42df979d2ac3dbb83da25f1f480
terjanq <terjanq@gmail.com>
2019-04-07 03:09:53 +0200

    strona janek

--- strona/example_receipt.js
+++ strona/example_receipt.js
@@ -1,73 +1,37 @@
-var RECEIPT_EXAMPLE = `
-    <div class="receipt">
-        <div class=receipt-top>
-            <div class=title>
-                Facebook
-            </div>
-            <div class=date>
-                03/03/2003
-            </div>
+var RECEIPT_TEMPLATE_HEADER = `
+<div class="receipt">
+    <div class=receipt-top>
+        <div class=title>
+            @SHOP@
         </div>
+        <div class=date>
+            @DATE@
+        </div>
+    </div>
+    @BODY@
+</div>
+`
+var RECEIPT_TEMPLATE_BODY = `
+<div class=receipt-body>
+    <div id="table">
+        <table>
+            @PRICES@
+        </table>
+    </div>
+</div>
+`;
 
-        <div class=receipt-body>
-            <div id="table">
-                <table>
-                    <tr class="service">
-                        <td class="tableitem">
-                            <p class="itemtext">Communication</p>
-                        </td>
-                        <td class="tableitem">
-                            <p class="itemtext">x5</p>
-                        </td>
-                        <td class="tableitem">
-                            <p class="itemtext">$375.00</p>
-                        </td>
-                    </tr>
-                    <tr class="service">
-                        <td class="tableitem">
-                            <p class="itemtext">Asset Gathering</p>
-                        </td>
-                        <td class="tableitem">
-                            <p class="itemtext">x3</p>
-                        </td>
-                        <td class="tableitem">
-                            <p class="itemtext">$225.00</p>
-                        </td>
-                    </tr>
-                    <tr class="service">
-                        <td class="tableitem">
-                            <p class="itemtext">Design Development</p>
-                        </td>
-                        <td class="tableitem">
-                            <p class="itemtext">x5</p>
-                        </td>
-                        <td class="tableitem">
-                            <p class="itemtext">$375.00</p>
-                        </td>
-                    </tr>
-                    <tr class="service">
-                        <td class="tableitem">
-                            <p class="itemtext">Animation</p>
-                        </td>
-                        <td class="tableitem">
-                            <p class="itemtext">x20</p>
-                        </td>
-                        <td class="tableitem">
-                            <p class="itemtext">$1500.00</p>
-                        </td>
-                    </tr>
-                    <tr class="service">
-                        <td class="tableitem">
-                            <p class="itemtext">Animation Revisions</p>
-                        </td>
-                        <td class="tableitem">
-                            <p class="itemtext">x10</p>
-                        </td>
-                        <td class="tableitem">
-                            <p class="itemtext">$750.00</p>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    </div>`
+var RECEIPT_TEMPLATE_PRICE = `
+<tr class="service">
+    <td class="tableitem">
+        <p class="itemtext">@PRODUCT@</p>
+    </td>
+    <td class="tableitem">
+        <p class="itemtext">x@QUANTITY@</p>
+    </td>
+    <td class="tableitem">
+        <p class="itemtext">$@PRICE@</p>
+    </td>
+</tr>
+
+`

d5b8d024b3fd58666060e1ad7aa61fb93b500cd8
Michał Jaworski <jawor403@gmail.com>
2019-04-07 03:44:09 +0200

    Firefox śmierdzi

--- app/public/scripts/charts/tag_bar.js
+++ app/public/scripts/charts/tag_bar.js
@@ -1,3 +1,41 @@
+function f_tag_bar(chart_data){
+    return {
+        type: 'bar',
+        data: {
+            datasets: [{
+                data: chart_data,
+                backgroundColor: [
+                    "rgb(255, 99, 132)",
+                    "rgb(255, 159, 64)",
+                    "rgb(255, 205, 86)",
+                ],
+                // label: 'Food'
+            }],
+            labels: [
+                'You',
+                'Friends',
+                'Local',
+            ]
+        },
+        options: {
+            legend: {
+                display: false
+            },
+            scales: {
+                xAxes: [{
+                    barPercentage: 0.5,
+                    // barThickness: 30,
+                    maxBarThickness: 100,
+                    minBarLength: 10,
+                    gridLines: {
+                        offsetGridLines: true
+                    }
+                }]
+            }
+        }
+    };
+}
+
 var tag_bar =  {
     type: 'bar',
     data: {
